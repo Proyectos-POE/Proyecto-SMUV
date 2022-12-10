21d27c72838c1c9b35582f954e823c85e75f5cc9
Nicolas Herrera <herreran903@gmail.com>
2022-12-10 11:28:45 -0500

    SMUV-34-crear-clase-persona Se creo la clase Persona y sus respectivos metodos.

---
+++ src/main/java/modelo/Persona.java
@@ -0,0 +1,48 @@
+package modelo;
+
+public class Persona
+{
+    protected String nombre;
+    protected Documento documento;
+    protected String correo;
+    protected long telefono;
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String auxNombre)
+    {
+        this.nombre = auxNombre;
+    }
+
+    public Documento getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(Documento auxDocumento)
+    {
+        this.documento = auxDocumento;
+    }
+
+    public String getCorreo()
+    {
+        return correo;
+    }
+
+    public void setCorreo(String auxCorreo)
+    {
+        this.correo = auxCorreo;
+    }
+
+    public long getTelefono()
+    {
+        return telefono;
+    }
+
+    public void setTelefono(long auxTelefono)
+    {
+        this.telefono = auxTelefono;
+    }
+}
