f070ce2a93f7792d572c56cb5518044a02d20e07
Julian <juldarecar2003@gmail.com>
2022-12-27 19:10:21 -0500

    Actualización del Main del proyecto

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -8,6 +8,12 @@ import modelo.Conexion;
 import modelo.Empresa;
 import modelo.Medico;
 
+/* 
+ * @author Nicolas Herrera <herrera.nicolas@correounivalle.edu.co>
+ * @author Samuel Galindo Cuevas <samuel.galindo@correounivalle.edu.co>
+ * @author Julian Rendon <julian.david.rendon@correounivalle.edu.co>
+ */
+
 public class Main
 {
     public static void main(String[] args)
