f6e8bf57c1a4dbcf49dcab1663c91289c522a65f
Julian <juldarecar2003@gmail.com>
2022-12-27 19:10:01 -0500

    Actualización de los Dao del proyecto

--- src/main/java/dao/CitaDao.java
+++ src/main/java/dao/CitaDao.java
@@ -4,6 +4,12 @@ import modelo.*;
 
 import java.util.ArrayList;
 
+/* 
+ * @author Nicolas Herrera <herrera.nicolas@correounivalle.edu.co>
+ * @author Samuel Galindo Cuevas <samuel.galindo@correounivalle.edu.co>
+ * @author Julian Rendon <julian.david.rendon@correounivalle.edu.co>
+ */
+
 public class CitaDao
 {
     private final ArrayList<Cita> citas;
